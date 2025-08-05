158a4acde171874a20bae3d51ebe33eb4e4022fe
FatmaAHassan <your.email@example.com>
2025-08-05 06:20:44 +0300

    first commit

---
+++ lib/core/config/app_padding.dart
@@ -0,0 +1,4 @@
+class AppPadding {
+  static const double horizontalPagePadding = 30;
+  static const double containerPaddingAll = 16;
+}
---
+++ lib/core/config/app_palette.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppPalette {
+  static const backgroundLight = Colors.white;
+  
+}
